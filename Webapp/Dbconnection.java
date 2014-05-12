b1c1689593eef65e17bc7d84cc363e0e77040c70
Andres van Lummel <andresvanlummel@hotmail.com>
2014-05-12 18:58:37 +0200

    added some imports

--- Webapp/Dbconnection.java
+++ Webapp/Dbconnection.java
@@ -1,41 +1,38 @@
 package Webapp;
 
-import java.sql.DriverManager;
+import java.sql.*;
 import java.sql.SQLException;
-import com.mysql.jdbc.Connection;
 
 public class Dbconnection {
 	private static Connection con = null;
-	static{ 
-		
+	static {
+
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			
+
 			System.out.println("Driver found");
 		} catch (ClassNotFoundException e) {
 			System.out.println("Driver not found: " + e);
 		}
-		
-		String url="jdbc:mysql://localhost/autototaaldienst";
-		String user="root";
-		String password="";
-		
-		
-		
-		try{
-		con=(Connection) DriverManager.getConnection(url, user, password);
-		System.out.println("Connectie is goed");
-		}catch (SQLException e){
+
+		String url = "jdbc:mysql://localhost/autototaaldienst";
+		String user = "root";
+		String password = "";
+
+		try {
+			con = (Connection) DriverManager.getConnection(url, user, password);
+			System.out.println("Connectie is goed");
+		} catch (SQLException e) {
 			System.out.println("Er gaat iets verkeerd");
 		}
 	}
-	
-	public Connection getConnection(){
+
+	public Connection getConnection() {
 		return con;
 	}
-	
-	public static void saveUser(User u){
-		
+
+	public static void saveUser(User u) {
+
 	}
 
 }
--- Webapp/LoginServlet.java
+++ Webapp/LoginServlet.java
@@ -1,20 +1,10 @@
 package Webapp;
 
 import java.io.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Date;
+import java.sql.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 public class LoginServlet extends HttpServlet {
 	String userName = "1", userPass = "1", id = "";
