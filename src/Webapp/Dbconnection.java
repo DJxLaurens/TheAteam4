c872837a62a71ac399c29b721e1fa38b6964807e
Andres van Lummel <andresvanlummel@hotmail.com>
2014-05-15 14:22:08 +0200

    Logincheck doesnt work jet

--- src/Webapp/Dbconnection.java
+++ src/Webapp/Dbconnection.java
@@ -7,11 +7,11 @@ public class Dbconnection {
 	private static Statement statement;
 	private static ResultSet resultSet = null;
 	private static int counter = 1;
+	private static String naam;
 	static {
 
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-
 			System.out.println("Driver found");
 		} catch (ClassNotFoundException e) {
 			System.out.println("Driver not found: " + e);
@@ -26,23 +26,15 @@ public class Dbconnection {
 			System.out.println("Connectie is goed");
 		} catch (SQLException e) {
 			System.out.println("Er gaat iets verkeerd");
-			e.printStackTrace();
-		}
-		try {
-			resultSet = con.createStatement().executeQuery("select * from autotaaldiensten.user");			
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			e.printStackTrace();			
 		}
 	}
 
-	public static void saveUser(User u) throws SQLException {
+	public static void saveUser(User u) throws SQLException {	
 		statement = con.createStatement();
-		while(resultSet.next()){
-			counter ++;
-		}
-		try {
-			statement.execute("INSERT INTO user (id, name, role_id, address, email, password)"+ "VALUES ('"+counter+"','"+u.getName() + "','"+ u.getRol()+ "','"+ u.getAddress() + "', '"+ u.getEmail()+ "','"+ u.getPass() + "')");
+		try {			
+			statement.execute("INSERT INTO user (id, name, role_id, address, email, password)"+ "VALUES ('"+counter+"','"+ u.getName() + "','"+ u.getRol()+ "','"+ u.getAddress() + "', '"+ u.getEmail()+ "','"+ u.getPass() + "')");			
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
@@ -53,11 +45,9 @@ public class Dbconnection {
 				e.printStackTrace();
 			}
 		}
-
+		
 	}
-
-	public static ResultSet resultset() throws SQLException{
-		System.out.println("Dit wordt opgehaald uit de database: " + resultSet);
+	public static ResultSet resultset(){
 		return resultSet;
 		
 	}
