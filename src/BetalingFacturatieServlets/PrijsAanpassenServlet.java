a1f1d75171fda47a05382087c61d1511f0f4dbbc
Laurens Verkerk <laurens@lverkerk.nl>
2014-06-24 13:29:10 +0200

    Set PrijsAanpassenServlet back

--- src/BetalingFacturatieServlets/PrijsAanpassenServlet.java
+++ src/BetalingFacturatieServlets/PrijsAanpassenServlet.java
@@ -1,7 +1,6 @@
 package BetalingFacturatieServlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.SQLException;
 
 import javax.servlet.RequestDispatcher;
@@ -47,143 +46,51 @@ public class PrijsAanpassenServlet extends HttpServlet{
 			double wp = 0;
 			double mp = 0;
 
-//			if(isDouble(v1) && v2.equals("") && v3.equals("") && v4.equals("") && v5.equals("")){
-//				lp = Double.parseDouble(v1);
-//				if(lp > 0){
-//					atd.setLiterPrijs(lp);
-//					PrintWriter out = resp.getWriter();
-//					out.println("<script type=\"text/javascript\">");
-//					out.println("alert('Literprijs is aangepast!');");  
-//					out.println("window.location = 'prijsaanpassen.jsp'");
-//					out.println("</script>");
-//					out.close();
-//				}else{
-//					lp = atd.getLiterPrijs();
-//					PrintWriter out = resp.getWriter();
-//				    out.println("<script type=\"text/javascript\">");
-//				    out.println("alert('Vul bij literprijs een getal boven de 0 in!');");  
-//				    out.println("window.location = 'prijsaanpassen.jsp'");
-//				    out.println("</script>");
-//				    out.close();
-//				}
-//			} 
-//			
-//			if(v1.equals("") && isDouble && v3.equals("") && v4.equals("") && v5.equals("")){
-//				lp = Double.parseDouble(v1);
-//				if(lp > 0){
-//					atd.setLiterPrijs(lp);
-//					PrintWriter out = resp.getWriter();
-//					out.println("<script type=\"text/javascript\">");
-//					out.println("alert('Literprijs is aangepast!');");  
-//					out.println("window.location = 'prijsaanpassen.jsp'");
-//					out.println("</script>");
-//					out.close();
-//				}else{
-//					lp = atd.getLiterPrijs();
-//					PrintWriter out = resp.getWriter();
-//				    out.println("<script type=\"text/javascript\">");
-//				    out.println("alert('Vul bij literprijs een getal boven de 0 in!');");  
-//				    out.println("window.location = 'prijsaanpassen.jsp'");
-//				    out.println("</script>");
-//				    out.close();
-//				}
-//			} 
-			
 			if(isDouble(v1)){
-				lp = atd.getLiterPrijs();
-				PrintWriter out = resp.getWriter();
-				out.println("<script type=\"text/javascript\">");
-				out.println("alert(Prijs is aangepast!');");  
-				out.println("window.location = 'prijsaanpassen.jsp'");
-				out.println("</script>");
-				out.close();
-			} else {
+				lp = Double.parseDouble(v1);
+			} else{
 				lp = atd.getLiterPrijs();
 			}
 
 			if(isDouble(v2)){
 				mup = Double.parseDouble(v2);
-				PrintWriter out = resp.getWriter();
-				out.println("<script type=\"text/javascript\">");
-				out.println("alert(Prijs is aangepast!');");  
-				out.println("window.location = 'prijsaanpassen.jsp'");
-				out.println("</script>");
-				out.close();
 			} else{
 				mup = atd.getWerkUurPrijs();
 			}
 
 			if(isDouble(v3)){
 				dp = Double.parseDouble(v3);
-				PrintWriter out = resp.getWriter();
-				out.println("<script type=\"text/javascript\">");
-				out.println("alert(Prijs is aangepast!');");  
-				out.println("window.location = 'prijsaanpassen.jsp'");
-				out.println("</script>");
-				out.close();
 			} else{
 				dp = atd.getDagPrijs();
 			}
 
 			if(isDouble(v4)){
 				wp = Double.parseDouble(v4);
-				PrintWriter out = resp.getWriter();
-				out.println("<script type=\"text/javascript\">");
-				out.println("alert(Prijs is aangepast!');");  
-				out.println("window.location = 'prijsaanpassen.jsp'");
-				out.println("</script>");
-				out.close();
 			} else{
 				wp = atd.getWeekPrijs();
 			}
 
 			if(isDouble(v5)){
 				mp = Double.parseDouble(v5);
-				PrintWriter out = resp.getWriter();
-				out.println("<script type=\"text/javascript\">");
-				out.println("alert(Prijs is aangepast!');");  
-				out.println("window.location = 'prijsaanpassen.jsp'");
-				out.println("</script>");
-				out.close();
 			} else{
 				mp = atd.getMaandPrijs();
 			}
-			
-//			if(isDouble(v1) || isDouble(v2) || isDouble(v3) || isDouble(v4) || isDouble(v5)){
-//				PrintWriter out = resp.getWriter();
-//				out.println("<script type=\"text/javascript\">");
-//				out.println("alert(Prijs is aangepast!');");  
-//				out.println("window.location = 'prijsaanpassen.jsp'");
-//				out.println("</script>");
-//				out.close();
-//			} else {
-//				PrintWriter out = resp.getWriter();
-//				out.println("<script type=\"text/javascript\">");
-//				out.println("alert(Vul een getal boven de 0 in!');");  
-//				out.println("window.location = 'prijsaanpassen.jsp'");
-//				out.println("</script>");
-//				out.close();
-//			}
 
 			if(lp > 0){
 				atd.setLiterPrijs(lp);
-			} 
-			
+			}
 			if(mup > 0){
 				atd.setWerkUurPrijs(mup);
-			} 
-			
+			}
 			if(dp > 0){
 				atd.setDagPrijs(dp);
-			} 
-			
+			}
 			if(wp > 0){
 				atd.setWeekPrijs(wp);
-			} 
-			
+			}
 			if(mp > 0){
 				atd.setMaandPrijs(mp);
-			} 
+			}
 			
 			try {
 				new AutoTotaalDienstenDAO().deleteAutoTotaalDienst(1);
