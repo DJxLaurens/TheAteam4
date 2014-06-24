76d1fbbe48290fc0cbc08361c008020df9caa6b3
theolice <theoflorea@hotmail.com>
2014-06-24 13:35:00 +0200

    AutoTotaalDienstenDAOTest aangemaakt

---
+++ src/TestsDAO/AutoTotaalDienstenDAOTest.java
@@ -0,0 +1,29 @@
+package TestsDAO;
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import DAO.AutosDAO;
+import DAO.GebruikersDAO;
+
+public class AutoTotaalDienstenDAOTest {
+	AutosDAO aDao = new AutosDAO();
+	GebruikersDAO gDao = new GebruikersDAO();
+	
+	@Before
+	public void setUp(){		
+	}
+	
+	@Test
+	public void getATD(){
+	}
+	
+	@Test
+	public void saveAutoTotaalDienst(){
+	}
+}
